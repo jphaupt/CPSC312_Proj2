1b968d71c2ce3241c27e1a5622f86a6eafa15801
jphaupt <jphiliphaupt@gmail.com>
2018-11-26 20:49:34 -0800

    pair programing

--- aco.pl
+++ aco.pl
@@ -1,26 +1,53 @@
-https://www.aransena.com/blog/2015/12/22/python-ant-colony-optimization
+% https://www.aransena.com/blog/2015/12/22/python-ant-colony-optimization
+
+:- use_module(library(random)).
+%:- use_module(library(plrand)).
+
 
 
 % Represents the city, (x,y) coordinates
-city('Vancouver', 3, 5).
-city('Montreal', 7, 9).
-city('Toronto', 2, 8).
-city('Calgary', 8, 11).
-city('Ottawa', 9, 13).
+% city(name, x, y, pheromone_level)
+city('Vancouver', 3, 5). % 0
+city('Montreal', 7, 9). % 1
+city('Toronto', 2, 8). % 2 
+city('Calgary', 8, 11). % 3
+city('Ottawa', 9, 13). % 4
+
+%[[0, 3, 4]
+%[3, 0, 1]
+%[4, 1, 0]]
+
+%select_city(start_city, end_city) 
+%start_city == 0
+%end_city in [1,2]
+
+%city('YVR', 3, 5, P+1) :- ant_visit(city('YVR', 3, 5, P), true)
 
 % assume only one ant for now
 
-find_next_city(Ant, CurrentCity, Visited, Path) :-
+%find_next_city(Ant, CurrentCity, Visited, Path) :-
   % Find distance between CurrentCity and all other cities -- find minimum or if
   % then use probability
 
 % Need to somehow represent ants
 % also need to represent the current city
-ant :: {
-  city('Vancouver', 3, 5) &
-  distance(0) &
-  visited([])
-}.
+%ant :: {
+%  city('Vancouver', 3, 5) &
+%  distance(0) &
+%  visited([])
+%}.
+
+
+
+% pheromone deposit
+%pher_dep(ant(city(N, _, _, P), _, _), city(N, _, _, P+1)).
+
+
+%tour_ant(lst
+
+%tour_all(
+
+%aco_tsp(
 
 % for ant in ants:
 %    current_city = ant.current_city
@@ -35,3 +62,51 @@ ant :: {
 %        transition_probabilities.append(p_ij)
 %
 %    next_city = numpy.choice(unvisited, 1, transition_probabilities)
+
+
+
+
+%%%% HELPER FUNCTIONS %%%%
+% replace Ith item in L with E (this is K) 
+replace(I, L, E, K) :-
+  nth1(I, L, _, R),
+  nth1(I, K, E, R).
+
+% fancy schmancy matrix creation function
+make_sq_zero_matrix(N, Matrix) :-
+    make_matrix(N, N, Matrix).
+
+make_zero_matrix(_, N, []) :-
+    N =< 0,
+    !. % ??? TODO
+make_zero_matrix(M, N, [R|Rs]) :-
+    make_zero_list(M, R),
+    N2 is N - 1,
+    make_matrix(M, N2, Rs).
+
+% make list of zeros
+make_zero_list(N, []) :-
+    N =< 0,
+    !.
+make_zero_list(N, [0|Rest]) :-
+    N > 0,
+    N2 is N - 1,
+    make_list(N2, Rest).
+
+% starting pheromone matrix
+% make_sq_zero_matrix(num_cities, Pher_matr)
+
+% TODO adjacency matrix
+
+% distance squared
+dist2(city(_, X1, Y1), city(_, X2, Y2), D) :- D is (X1-X2)^2+(Y1-Y2)^2.
+
+% find argmax (0 indexing)
+argmax(Lst, Ind) :-
+  max_list(Lst, Max),
+  nth0(Ind, Lst, Max).
+
+% choose based on values in Prob (return index)
+choice(Probs, Ind) :-
+  
+
